8ecc56aceb5121bf063a304e82c7f1ad25f0d185
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-26 10:40:47 +0530

    Update ProductController.java

--- src/main/java/in/eyesoncloud/controller/ProductController.java
+++ src/main/java/in/eyesoncloud/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import in.ashokit.binding.Product;
+import in.eyesoncloud.binding.Product;
 
 @Controller
 public class ProductController {
